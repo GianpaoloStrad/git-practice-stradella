4c67135667d91e800a71ec9721a09d64a7fac985
GianpaoloStrad <gianpaolo.30338054@uru.edu>
2026-01-30 14:36:28 -0400

    feat: Primer programa Hola mundo

---
+++ hack1.cpp
@@ -0,0 +1,8 @@
+//hello world
+#include <iostream> 
+using namespace std;
+int main () {
+    cout << "Hello, World from Git!" << endl;
+    cout << "My name is Gianpaolo Stradella" << endl;
+    return 0;
+}
