e914a82667239338aa82aaad6599787f0c2f24b3
GianpaoloStrad <gianpaolo.30338054@uru.edu>
2026-01-30 14:39:13 -0400

    feat: agregue mas mensajes a mi programa

--- hack1.cpp
+++ hack1.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main () {
     cout << "Hello, World from Git!" << endl;
     cout << "My name is Gianpaolo Stradella" << endl;
+    cout << "I am learning Programming 2" << endl;
+    cout << "This is my second commit" << endl;
     return 0;
 }
