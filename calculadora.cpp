6b986f290bfc55bab9f19e74eff32f403d0c2cb8
GianpaoloStrad <gianpaolo.30338054@uru.edu>
2026-02-03 21:07:17 -0400

    feat: agregar calculadora basica

---
+++ calculadora.cpp
@@ -0,0 +1,12 @@
+//calculadora.cpp
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "5 + 3 = " << sum(5, 3) << endl;
+    return 0;
+}
